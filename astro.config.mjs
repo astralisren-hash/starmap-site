6a01cf9f436cbd4a1e5cabd68bd36ad30e505bc5
astralisrenstar <astralisren@gmail.com>
2026-02-20 08:19:46 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  site: "https://www.starmap.direct",
-  trailingSlash: "ignore",
-  integrations: [react()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
